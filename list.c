25d267cf21e25379d683181ec70871202c4662cf
Kuriko <kurikokirei@gmail.com>
2023-11-16 11:46:34 +0800

    Added few print statements!

--- list.c
+++ list.c
@@ -14,13 +14,14 @@ typedef struct
     float total_price;
 } OrderedOrder;
 
-void append_array(int *last_index, OrderedOrder orders[], OrderedOrder order_to_be_added);
+void add_order(int *last_index, OrderedOrder orders[], Order order, int quantity);
 OrderedOrder make_order(Order order, int quantity);
 float get_price(Order order, int quantity);
 float get_total_price(OrderedOrder orders[], int size);
 
 void main()
 {
+    int size = 0;
     Order frappucino = {"Frappucino", 69.99};
     Order kopikoBrown = {"Kopiko Brown", 10.00};
     Order milo = {"Milo", 20.00};
@@ -30,20 +31,22 @@ void main()
     Order orders[] = {frappucino, kopikoBrown, milo, pepsi, coke};
     OrderedOrder orderedOrder[] = {};
     printf("\n   Name: \t\t Price: \t Quantity:\n");
-    int size = 0;
-    append_array(&size, orderedOrder, make_order(frappucino, 3));
-    append_array(&size, orderedOrder, make_order(coke, 5));
+    add_order(&size, orderedOrder, frappucino, 2);
+    add_order(&size, orderedOrder, kopikoBrown, 100);
+    add_order(&size, orderedOrder, milo, 20);
+    add_order(&size, orderedOrder, coke, 1000);
     for (int i = 0; i < size; i++)
     {
-        printf("\n%d: %-14s\t %.2f php \t %d\t %.2f php\n", i + 1, orderedOrder[i].order.name_of_order,
+        printf("\n%d: %-14s\t %.2f php \t %d\t  |    %.2f php\n", i + 1, orderedOrder[i].order.name_of_order,
                orderedOrder[i].order.price, orderedOrder[i].quantity, orderedOrder[i].total_price);
     }
-    printf("\nTotal: %.2f php", get_total_price(orderedOrder, size));
+    printf("-----------------------------------------------------------------------");
+    printf("\n\n\t\t\t\t\t\tTotal: %.2f php", get_total_price(orderedOrder, size));
 }
 
-void append_array(int *last_index, OrderedOrder orders[], OrderedOrder order_to_be_added)
+void add_order(int *last_index, OrderedOrder orders[], Order order, int quantity)
 {
-    orders[*last_index] = order_to_be_added;
+    orders[*last_index] = make_order(order, quantity);
     *last_index += 1;
 }
 
